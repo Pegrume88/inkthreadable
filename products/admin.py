e910db65eda1f4e412ae0f43d04f02ba95b528c8
Pegrume88 <pegrume88@gmail.com>
2023-01-31 13:17:36 +0000

    customized admin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
 from .models import Product, Category, Review
 
-admin.site.register(Product)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'total_rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'review_star',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Review)
--- products/models.py
+++ products/models.py
@@ -2,14 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-
-#class Gender(Choices):
-    #male = Choice("male")
-   # female = Choice("female")
-   # not_specified = Choice("not specified")
-
-
 class Category(models.Model):
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -37,9 +34,8 @@ class Product(models.Model):
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
-
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
 
 class Review(models.Model):
