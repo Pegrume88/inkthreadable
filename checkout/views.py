c37fa7577c48a2ce63e3b2f870965790f302d0cb
Pegrume88 <pegrume88@gmail.com>
2023-02-13 11:14:07 +0000

    added stripe elememnts and styling

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MMUpWKYuNOljO3YrQzg7osGRgpW2QX7uaxXj7rbDbdb1VAzctu4wMJMjHNVSQFK5k6238uyuPrZRezumOozf7Sr00hpxTTw1s',
+        'client_secret': 'test client secret'
         
     }
 
