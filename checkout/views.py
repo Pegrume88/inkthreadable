e8a0634956894605f1dd4e3d8644544b10810742
Pegrume88 <pegrume88@gmail.com>
2023-02-13 12:37:44 +0000

    added checkout functionality

--- checkout/views.py
+++ checkout/views.py
@@ -1,20 +1,38 @@
 from django.shortcuts import render, redirect, reverse
+from django.conf import settings
 from django.contrib import messages
-
+from cart.contexts import cart_contents
 from .forms import OrderForm
+import stripe
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+    
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "There are No items in your cart")
-        return redirect(reverse('products'))
+        return redirect(reverse('products')
+                        )
+    current_cart = cart_contents(request)
+    total = current_cart['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+    print(intent)
     
     order_form = OrderForm()
+    if not stripe_public_key:
+        messages.warning(request, 'stripe public key is missing,\
+            did you forget to set it in your environment')
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51MMUpWKYuNOljO3YrQzg7osGRgpW2QX7uaxXj7rbDbdb1VAzctu4wMJMjHNVSQFK5k6238uyuPrZRezumOozf7Sr00hpxTTw1s',
-        'client_secret': 'test client secret'
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
         
     }
 
--- inkthreadable/settings.py
+++ inkthreadable/settings.py
@@ -12,8 +12,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+
+if os.path.exists("env.py"):
+    import env
+    
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -188,5 +193,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+#stripe
+
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
