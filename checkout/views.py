ee7ecc79c8e79be17783c179f340418ab6ed464c
Pegrume88 <pegrume88@gmail.com>
2023-02-14 14:42:51 +0000

    updated webhook handlers to handle profiles

--- checkout/views.py
+++ checkout/views.py
@@ -27,11 +27,11 @@ def cache_checkout_data(request):
         messages.error(request, 'Sorry, your payment cannot be \
             processed right now. Please try again later.')
         return HttpResponse(content=e, status=400)
-    
+
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
-    
+
     if request.method == 'POST':
         cart = request.session.get('cart', {})
         form_data = {
