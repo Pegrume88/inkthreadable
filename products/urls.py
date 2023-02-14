e6bee9074aed7a33143b58df0237c928a39979d5
Pegrume88 <pegrume88@gmail.com>
2023-02-14 17:52:02 +0000

    product admin - edit product added

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('category/<category_id>', views.category_products, name='category_products'),
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),   
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),   
     ]
