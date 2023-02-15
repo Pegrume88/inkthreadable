16692c88a6a075f43dcea8d1133edbcdd1173221
Pegrume88 <pegrume88@gmail.com>
2023-02-15 16:42:14 +0000

    deploy attempt

--- inkthreadable/settings.py
+++ inkthreadable/settings.py
@@ -187,6 +187,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
